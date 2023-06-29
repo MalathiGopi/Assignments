83f299e091ab8402ec21630369ddfbab53aba573
MalathiGopi <malathigopi1997@gmail.com>
2023-06-29 13:11:28 +0530

    day 1 code

---
+++ 100_days_Of_Code_Challenge/Day_1_Band_Name_Generator/band_game.py
@@ -0,0 +1,24 @@
+#Create a greeting for your program
+print("Hello Welcome to the Band Name Generator")
+#Ask the User for the City that they grewup in 
+city=input("What's the name of the city you grew up in ?\n")#Ask the User for the name of the Pet
+
+##combine city and pet to show up the band name
+
+pet=input("What's your pet's name ?\n")
+
+print("Your Band name could be : "+city+" "+pet)
+
+print("Thanks for using our Band Name Generator Service")
+
+'''
+Output:
+=======
+Hello Welcome to the Band Name Generator
+What's the name of the city you grew up in ?
+london
+What's your pet's name ?
+pristo
+Your Band name could be : london pristo
+Thanks for using our Band Name Generator Service
+'''
